497ab3114d89b5c91252af28518c7d69f2afe4aa
Carlos Fernandez <carlos@allismymusic.org>
2014-08-01 00:15:13 +0200

    Draft of an FTP read client.
    Only meant to access and list the contents of an FTP server.
    
    Could be expanded in the future.

---
+++ source/SynoDs.Core.Interfaces/IFtpClient.cs
@@ -0,0 +1,21 @@
+﻿namespace SynoDs.Core.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Basic interface to work with a very basic FTP client.
+    /// It is not intended to be a full working client. Just read only operations.
+    /// It is up to the client to store the information.
+    /// </summary>
+    public interface IFtpClient
+    {
+        string UserName { get; set; }
+        string Password { get; set; }
+        Uri HostUri { get; set; }
+        void ConnectAsync();
+        void DisconnectAsync();
+        Task<IList<string>> ListFolderContentAsync(string path);
+    }
+}
