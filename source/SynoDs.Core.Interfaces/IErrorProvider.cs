f0651137b1268d659152b6c53d1a2817ad8f5350
Carlos Fernandez <carlos@allismymusic.org>
2014-08-12 23:43:13 +0200

    Some error interfaces
    Basic interfaces to get some known errors handling.

--- source/SynoDs.Core.Interfaces/IErrorProvider.cs
+++ source/SynoDs.Core.Interfaces/IErrorProvider.cs
@@ -6,10 +6,15 @@
     public interface IErrorProvider
     {
         /// <summary>
+        /// It's our source of information on known error types.
+        /// </summary>
+        IErrorRepository ErrorRepository { get; }
+
+        /// <summary>
         /// Given the error code, this method will return the error description. 
         /// </summary>
         /// <param name="errorCode">Synology API error code.</param>
         /// <returns>String with error description.</returns>
-        string GetErrorDescriptionForType<T>(int errorCode);
+        string GetErrorDescriptionForType(int errorCode);
     }
 }
---
+++ source/SynoDs.Core.Interfaces/IErrorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SynoDs.Core.Interfaces
+{
+    /// <summary>
+    /// Interface for accessing the Error data.
+    /// </summary>
+    public interface IErrorRepository
+    {
+        IDictionary<int, string> ErrorDictionary { get; set; } 
+    }
+}
