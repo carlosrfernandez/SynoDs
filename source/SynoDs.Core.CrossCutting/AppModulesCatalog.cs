99dd263bbbec7a03ad3924c9ecddf589dd610e93
Carlos Fernandez <carlos@allismymusic.org>
2015-02-23 23:06:45 +0100

    added module class

---
+++ source/SynoDs.Core.CrossCutting/AppModulesCatalog.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using SynoDs.Core.Contracts.IoC;
+using SynoDs.Core.Contracts.Modularity;
+
+namespace SynoDs.Core.CrossCutting
+{
+    // TODO: this has to be implemented by 
+    public abstract class AppModulesCatalog : IApiModuleCatalog
+    {
+        private readonly IEnumerable<IApiModule> _applicationModules; 
+        // This will be implemented on a per platform project
+        public AppModulesCatalog(IEnumerable<IApiModule> appModules)
+        {
+            _applicationModules = appModules;
+        }
+
+        public void InitCatalog()
+        {
+            foreach (var module in _applicationModules)
+            {
+                module.Configure();
+            }
+        }
+    }
+}
