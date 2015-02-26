70e5f00e735fa8350d110aa2f05d286e84bd34dc
Carlos Fernandez <carlos@allismymusic.org>
2015-02-27 00:16:30 +0100

    Fixed build issue in WinRT side because of Rename

--- source/SynoDs.Universal/SynoDs.Universal.Windows/LoginPage.xaml.cs
+++ source/SynoDs.Universal/SynoDs.Universal.Windows/LoginPage.xaml.cs
@@ -20,9 +20,9 @@ namespace SynoDs.Universal
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class LoginPage : Page
     {
-        public MainPage()
+        public LoginPage()
         {
             this.InitializeComponent();
         }
---
+++ source/SynoDs.Universal/SynoDs.Universal.WindowsPhone/LoginPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using SynoDs.Universal.Dtos;
+
+namespace SynoDs.Universal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var loginData = new LoginDto
+            {
+                Username = UsernameTextBox.Text,
+                Password = PasswordBox.Password,
+                RememberMe = RememberMeSwitch.IsOn,
+                Url = IpTextBox.Text,
+                UseSsl = UseSslSwitch.IsOn
+            };
+
+        }
+    }
+}
