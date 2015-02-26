9ec65cf5d9dad7034109794e7ee7a68056913a1a
Carlos Fernandez <carlos@allismymusic.org>
2015-02-27 00:13:02 +0100

    Windows Phone project now builds, fixed Ninject.
    also added basic design for login view.

--- source/SynoDs.Universal/SynoDs.Universal.Shared/App.xaml.cs
+++ source/SynoDs.Universal/SynoDs.Universal.Shared/App.xaml.cs
@@ -1,25 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
 using SynoDs.Core.Contracts.Modularity;
 using SynoDs.Core.CrossCutting;
 using SynoDs.Universal.Infrastructure;
+using SynoDs.Universal;
 
 namespace SynoDs.Universal
 {
@@ -114,7 +105,7 @@ namespace SynoDs.Universal
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
+                if (!rootFrame.Navigate(typeof(LoginPage), e.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
