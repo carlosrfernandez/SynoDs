6f51254566ffa89cf2a54ca0c4ae81913702f5de
Carlos Fernández <carlosrfa@outlook.com>
2016-03-10 20:13:51 +0100

    Added storage of password stuff in vault Added storage of connection

---
+++ source/SynoDs.UWP/Models/Connection.cs
@@ -0,0 +1,18 @@
+﻿using SynoDs.Core.Contracts.Synology;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynoDs.UWP.Models
+{
+    public class Connection
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool RememberMe { get; set; }
+        public bool UseSsl { get; set; }
+        public string Username { get; set; }
+    }
+}
--- source/SynoDs.UWP/ViewModels/LoginPageViewModel.cs
+++ source/SynoDs.UWP/ViewModels/LoginPageViewModel.cs
@@ -2,14 +2,19 @@
 using SynoDs.Core.CrossCutting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Template10.Mvvm;
 using Template10.Services.NavigationService;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.Practices.Unity;
 using System.Windows.Input;
+using Windows.Security.Credentials;
+using Windows.Storage;
 using Windows.UI.ViewManagement;
 using GalaSoft.MvvmLight.Command;
+using Newtonsoft.Json;
+using SynoDs.UWP.Models;
 
 namespace SynoDs.UWP.ViewModels
 {
@@ -20,19 +25,32 @@ namespace SynoDs.UWP.ViewModels
         /// </summary>
         private readonly IAuthenticationProvider authenticationProvider;
 
+        // todo: implement connection list
+        public List<Connection> ConnectionsList { get; set; }
+
         public string Host { get; set; }
 
         public string UserName { get; set; }
 
-        private string password { get; set; }
+        public string Password { get; set; }
 
-        private int port => 5001;
+        private int Port => 5001;
 
-        private bool useSsl { get; set; }
+        public bool UseSsl { get; set; }
 
         public bool StoreCredentials { get; set; }
 
         public ICommand LoginCommand { get; private set; }
+
+        private const string VaultResource = @"SynoDsCredStore";
+
+        private const string ConnectionContainerName = @"ConnectionsContainer";
+
+        private bool CredentialsRecovered
+        {
+            get; set;
+        }
+
 
         /// <summary>
         /// The empty constructor for mocking data.
@@ -46,6 +64,8 @@ namespace SynoDs.UWP.ViewModels
         {
             this.LoginCommand = new RelayCommand(async () => await LoginAsync(), () => true);
             this.authenticationProvider = authenticationProvider;
+            this.CredentialsRecovered = false;
+            this.RecoverCredentials();
         }
 
         public async Task LoginAsync()
@@ -55,8 +75,8 @@ namespace SynoDs.UWP.ViewModels
 #endif
             Views.Shell.SetBusy(true, "Loading...");
                 
-            var fullUrl = new Uri($"{Host}:{port}");
-            var loginResult = await this.authenticationProvider.LoginAsync(fullUrl, this.UserName, this.password);
+            var fullUrl = new Uri($"{Host}:{Port}");
+            var loginResult = await this.authenticationProvider.LoginAsync(fullUrl, this.UserName, this.Password);
             
             Views.Shell.SetBusy(false);
             //TODO: control error messages here.
@@ -66,7 +86,75 @@ namespace SynoDs.UWP.ViewModels
                 return;
             }
 
+            if (StoreCredentials && !this.CredentialsRecovered)
+            {
+                this.StoreCredentialsInVault();
+            }
+
+            Views.Shell.SetBusy(false);
+
             NavigationService.Navigate(typeof (Views.MainPage));
+        }
+
+        private void StoreCredentialsInVault()
+        {
+            var roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+
+            var container = roamingSettings.CreateContainer(ConnectionContainerName, Windows.Storage.ApplicationDataCreateDisposition.Always);
+
+            var connection = new Connection
+            {
+                Host = this.Host,
+                RememberMe = this.StoreCredentials,
+                UseSsl = this.UseSsl,
+                Username = this.UserName
+            };
+
+            // var dictionary = new Dictionary<string, Connection>();
+            // dictionary.Add(this.Host, new Connection { Host = this.Host, RememberMe = this.StoreCredentials, UseSsl = this.UseSsl });
+            var jsonStringConnection = JsonConvert.SerializeObject(connection);
+
+            container.Values.Add(this.Host, jsonStringConnection); 
+            var vault = new Windows.Security.Credentials.PasswordVault();
+            vault.Add(new PasswordCredential(this.Host, UserName, Password));
+        }
+
+        private void RecoverCredentials()
+        {
+            var roamingSettings = ApplicationData.Current.RoamingSettings;
+            var container = roamingSettings.CreateContainer(ConnectionContainerName,
+                ApplicationDataCreateDisposition.Always);
+
+            foreach (var kvp in container.Values)
+            {
+                this.Host = kvp.Key;
+                var connectionJson = kvp.Value as string;
+                if (connectionJson == null)
+                {
+                    continue;
+                }
+
+                var connection = JsonConvert.DeserializeObject<Connection>(connectionJson);
+
+                if (connection == null)
+                {
+                    continue;
+                }
+
+                if (!connection.RememberMe)
+                {
+                    continue;
+                }
+
+                var vault = new PasswordVault();
+                var password = vault.Retrieve(connection.Host, connection.Username);
+                this.Password = password.Password;
+                this.UserName = connection.Username;
+                this.StoreCredentials = connection.RememberMe;
+                this.UseSsl = connection.UseSsl;
+                this.CredentialsRecovered = true;
+                return;
+            }
         }
 
         private void NavigateToMainView()
